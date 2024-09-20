af9ef9f41d27f08912c5f59bc0c1fa2e93e5aeda
Michael Kuck <michaelkuck@MinivonMichael.fritz.box>
2024-09-20 18:57:55 +0200

    Enqueue Scripts

---
+++ functions.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+	Enqueue scripts and styles
+*/
+
+function hundimauto_assets() {
+
+	// Enqueue CSS Files
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap', array(), '1.0', 'all' );
+	wp_enqueue_style( 'bootstrap', get_theme_file_uri('bootstrap/dist/css/bootstrap.min.css'), array(), 'v5.3.3', 'all' );
+	wp_enqueue_style( 'flaticon', get_theme_file_uri('assets/font/flaticon.css'), array(), false, 'all' );
+	wp_enqueue_style( 'hamburgers', get_theme_file_uri('assets/css/hamburgers.min.css'), array(), false, 'all' );
+	wp_enqueue_style( 'animate-css', get_theme_file_uri('assets/css/animate.min.css'), array(), '4.1.1', 'all' );
+	wp_enqueue_style( 'swiper-bundle', get_theme_file_uri('assets/css/swiper-bundle.min.css'), array(), '11.1.9', 'all' );
+	wp_enqueue_style( 'hundimauto', get_stylesheet_uri(), array('bootstrap'), '1.0', 'all' );
+
+	// Enqueue JS Files
+	// Don't enqueue jQuery files it'll be loaded from WordPress when required
+	wp_enqueue_script( 'bootstrap', get_theme_file_uri('bootstrap/dist/js/bootstrap.min.js'), array(), 'v5.3.3', true );
+	wp_enqueue_script( 'jquery-waypoints', get_theme_file_uri('assets/js/jquery.waypoints.min.js"'), array('jquery'), '4.0.0', true );
+	wp_enqueue_script( 'swiper-bundle', get_theme_file_uri('assets/js/swiper-bundle.min.js"'), array(), '11.1.9', true );
+	wp_enqueue_script( 'froogaloop', get_theme_file_uri('assets/js/froogaloop.js"'), array(), false, true );
+	wp_enqueue_script( 'vimeo-player', get_theme_file_uri('assets/js/vimeo.player.2.24.0.min.js'), array(), 'v2.24.0', true );
+	wp_enqueue_script( 'hundimauto-js', get_theme_file_uri('assets/js/main-script.js'), array('jquery','jquery-ui-core','jquery-ui-selectmenu'), '1.0', true );
+	if ( is_singular() && comments_open() && get_option( 'thread_comments') ) {
+		wp_enqueue_script( 'comment-reply');
+	}
+
+}
+add_action( 'wp_enqueue_scripts', 'hundimauto_assets' );
+
+/*
+	Removing Emoji's from Code
+	https://fastwp.de/wordpress-emoji-support-deaktivieren/
+*/
+function remove_tinymce_emoji($plugins)
+{
+	if (!is_array($plugins)) { return array(); }
+	return array_diff($plugins, array('wpemoji'));
+}
+function remove_emoji()
+{
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	add_filter('tiny_mce_plugins', 'remove_tinymce_emoji');
+}
+add_action('init', 'remove_emoji');
+
+/*
+	Remove Classic themes.css completely
+	https://generatepress.com/forums/topic/remove-classic-themes-css-completely/
+*/
+add_action( 'wp_enqueue_scripts', function() {
+    wp_dequeue_style( 'classic-theme-styles' );
+    wp_dequeue_style( 'global-styles' );
+}, 20 );
---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+	<meta charset="utf-8">
+	<title>Hund im Auto</title>
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="color-scheme" content="light dark" />
+	
+	<?php wp_head(); ?>
+
+</head>
+<body class="home theme-default">
+
+	<header id="header">
+		<div id="theme">
+			<a href="/"><img id="logo" src="assets/images/logo-hia.svg" alt="Hund im Auto" /></a>
+		</div>
+		<div class="container">
+			<div class="row justify-content-end">
+				<div class="col-md-12">
+					<nav class="main-menu">
+						<ul class="navigation top-menu d-flex flex-row justify-content-end list-unstyled">
+							<li class="menu-item"><a href="index.html">Startseite</a></li>
+							<li class="menu-item menu-item-has-children"><a href="blog.html">Produkte nach Mass</a>
+								<ul class="sub-menu">
+									<li class="menu-item"><a href="index.html">Hundegitter nach Mass</a></li>
+									<li class="menu-item menu-item-has-children"><a href="index.html">Hundeboxen nach Mass</a>
+										<ul class="submenu">
+											<li class="menu-item"><a href="index.html">Unterseite</a></li>
+											<li class="menu-item"><a href="index.html">Unterseite</a></li>
+										</ul></li>
+									<li class="menu-item"><a href="index.html">Fahrzeugeinrichtungen nach Mass</a></li>
+									<li class="menu-item"><a href="index.html">Holzarbeiten nach Mass</a></li>
+									<li class="menu-item"><a href="index.html">Travelmat&copy; nach Mass</a></li>
+								</ul></li>
+							<li class="menu-item"><a href="herstellung.html">Herstellung</a></li>
+							<li class="menu-item"><a href="zubehoer.html">Zubehör</a></li>
+							<li class="menu-item special-menu"><a href="preise.html">Preise</a></li>
+							<li class="menu-item special-menu"><a href="offerte.html">Offerte</a></li>
+						</ul>
+					</nav>
+
+					<button type="button" class="mobile-nav-toggler hamburger hamburger--elastic" aria-label="Menu" aria-controls="navigation">
+						<span class="hamburger-label">Menu</span>
+						<span class="hamburger-box"><span class="hamburger-inner"></span></span>
+						
+					</button>
+				</div>
+			</div>
+
+			<!-- Mobile Menu -->
+			<div class="mobile-menu">
+				<nav class="menu-box">
+					<ul class="navigation clearfix"></ul>
+				</nav>
+			</div><!-- end Mobile Menu -->
+
+		</div>
+	</header><!-- end header parts -->
+
+	<section id="content">
+
+		<article id="intro" class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1 gx-5">
+					<hgroup>
+						<h1 class="os-animation" data-animation="animate__fadeInUp" data-delay=".3s"><a href="/">hundimauto.ch</a><span>&nbsp;&ndash;&nbsp;Hundegitter und Hundeboxen <strong>nach Mass</strong> in bewährter Schweizer Premium Qualität.</span></h1>
+						<h2 class="os-animation" data-animation="animate__fadeInUp" data-delay=".3s">Ihr Ansprechpartner für <a href="#">optimale Transportlösungen</a>, qualitativ <a href="#">hochwertig und fachmännisch</a> hergestellte <a href="#">Hundegitter</a> und <a href="#">Hundeboxen</a> nach Mass für Ihren <strong>HUND</strong> im <strong>AUTO</strong>.</h2>
+					</hgroup>
+					<div class="container-fluid g-0 contact os-animation" data-animation="animate__fadeInUp" data-delay=".6s">
+						<div class="row">
+							<div class="col-lg-3 col-md-12">
+								<button type="button" class="btn btn-primary">Offerte anfragen</button>
+							</div>
+							<div class="col-lg-9 d-none d-lg-block">
+								<p class="justify-right">oder kontaktieren Sie uns direkt unter <a href="tel:+41 44 875 02 22">044 875 02 22</a> oder <a href="mailto:info@hundimauto.ch">info@hundimauto.ch</a>.
+								<span class="note">Bei Kontaktaufnahme via E-Mail <strong>bitte immer</strong> eine Telefonnummer angeben.</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</article><!-- end section intro -->
+
+		<article class="container-fluid g-0 os-animation" data-animation="animate__fadeInUp" data-delay=".9s">
+
+			<div class="row">
+				<div class="col-12">
+					<div class="video-frame ratio ratio-16x9">
+						
+						<iframe id="video" src="https://player.vimeo.com/video/1001908180?autoplay=1&loop=1&color=d2ba8b&title=0&byline=0&portrait=0&muted=1" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+
+					</div>
+				</div>
+			</div>
+		</article><!-- end section video -->
+
+		<article id="testimonials" class="jumnbotron jumbotron-fluid">
+			<div class="container">
+
+				<div class="col-12">
+					<hgroup class="heading os-animation" data-animation="animate__fadeInUp" >
+						<h2>Kundenmeinungen</h2>
+					</hgroup>
+					<div class="swiper os-animation" data-animation="animate__fadeInUp" data-delay=".4s">
+						<div class="swiper-wrapper">
+
+							<div class="swiper-slide">
+								<div class="testimonial row">
+									<div class="col-lg-4">
+										<img src="assets/images/client2.jpg" alt="" />
+									</div>
+									<div class="col-lg-8">
+										<blockquote>
+											<img class="quote" src="assets/images/noun-quote-5767821.svg" alt="" />
+											Proin sagittis et nulla nec eleifend. Cras porttitor arcu id vestibulum maximus. Cras sed interdum neque, sit amet venenatis nulla. Aliquam sem dui, ornare a urna nec, pulvinar facilisis tellus.
+											<cite>&#8212; Alessa Stauffer, Redog</cite>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+
+							<div class="swiper-slide">
+								<div class="testimonial row">
+									<div class="col-lg-4">
+										<img src="assets/images/client2.jpg" alt="" />
+									</div>
+									<div class="col-lg-8">
+										<blockquote class="text-justify">
+											<img class="quote" src="assets/images/noun-quote-5767821.svg" alt="" />
+											Proin sagittis et nulla nec eleifend. Cras porttitor arcu id vestibulum maximus. Cras sed interdum neque, sit amet venenatis nulla.
+											<cite>&#8212; Alessa Stauffer, Redog</cite>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+
+							<div class="swiper-slide">
+								<div class="testimonial row">
+									<div class="col-lg-4">
+										<img src="assets/images/client2.jpg" alt="" />
+									</div>
+									<div class="col-lg-8">
+										<blockquote class="text-justify">
+											<img class="quote" src="assets/images/noun-quote-5767821.svg" alt="" />
+											Proin sagittis et nulla nec eleifend. Cras porttitor arcu id vestibulum maximus. Cras sed interdum neque, sit amet venenatis nulla. Aliquam sem dui, ornare a urna nec, pulvinar facilisis tellus.
+											<cite>&#8212; Alessa Stauffer, Redog</cite>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="swiper-pagination"></div>
+					</div><!-- end swiper -->
+				</div>
+			</div><!-- end container -->
+		</article><!-- end section testimonials -->
+
+		<article id="products" class="container-fluid px-0">
+			<div class="row justify-content-center">
+
+				<div class="col-12 os-animation" data-animation="animate__fadeInUp">
+					<hgroup class="heading">
+						<h2>Unsere Produkte</h2>
+					</hgroup>
+				</div>
+
+				<div class="row">
+
+					<div class="col-sm-12 col-md-6 p-0 os-animation" data-animation="animate__bounceInLeft" data-delay=".2s">
+						<div class="product-item overflow-hidden">
+							<img src="assets/images/picture-2.jpg" class="product-image w-100" />
+							<div class="overlay"><h3><a href="#">Hundegitter nach Mass</h3></a></div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 p-0 os-animation" data-animation="animate__bounceInRight" data-delay=".2s">
+						<div class="product-item overflow-hidden">
+							<img src="assets/images/picture-3.jpg" class="product-image w-100" />
+							<div class="overlay"><h3><a href="#">Hundeboxen nach Mass</h3></a></div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 p-0 os-animation" data-animation="animate__bounceInLeft" data-delay=".2s">
+						<div class="product-item overflow-hidden">
+							<img src="assets/images/picture-1.jpg" class="product-image w-100" />
+							<div class="overlay"><h3><a href="#">Fahrzeugausbauten nach Mass</h3></a></div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 p-0 os-animation" data-animation="animate__bounceInRight" data-delay=".2s">
+						<div class="product-item overflow-hidden">
+							<img src="assets/images/picture-4.jpg" class="product-image w-100" />
+							<div class="overlay"><h3><a href="#">Holzarbeiten nach Mass</h3></a></div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</article><!-- end section products -->
+
+		<article class="get-offer container-fluid px-0">
+			<div class="row">
+				
+				<div class="col-12 os-animation" data-animation="animate__fadeInUp">
+					<hgroup class="heading">
+						<h2>Wünschen Sie eine unverbindliche Offerte?</h2>
+					</hgroup>
+				</div>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-2 offset-md-2 col-lg-3 offset-lg-1 cta os-animation" data-animation="animate__bounceInLeft">
+							<button type="button" class="btn btn-primary">Offerte anfragen</button>
+						</div>
+						<div class="col-sm-12 col-md-6 col-lg-7 os-animation" data-animation="animate__bounceInRight">
+							<p>oder kontaktieren Sie uns direkt unter <a href="tel:+41 44 875 02 22">044 875 02 22</a> oder <a href="mailto:info@hundimauto.ch">info@hundimauto.ch</a>.
+								<span class="note">Bei Kontaktaufnahme via E-Mail <strong>bitte immer</strong> eine Telefonnummer angeben.</span>
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</article>
+
+		<br /><br /><br /><br />
+
+		<article class="contents container">
+			<div class="row">
+				<div class="col-8 offset-2">
+					<h1>Ich bin eine Überschrift</h1>
+					<p class="lead">Lorem ipsum dolor sit amet, <a href="#">consectetur adipiscing elit</a>. Pellentesque sit amet erat tincidunt, maximus magna eget, porta quam. Maecenas consequat fermentum odio sit amet tincidunt.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet erat tincidunt, maximus magna eget, porta quam. Maecenas consequat fermentum odio sit amet tincidunt. Duis nec nisl mi. Mauris vestibulum purus lorem, quis sollicitudin enim tristique sit amet. Integer ultricies dui libero, nec tempor neque porttitor et. Donec hendrerit, purus et lacinia tincidunt, lorem purus commodo sem, et tincidunt velit lacus vitae neque. Integer neque mauris, volutpat sed scelerisque sit amet, maximus vel metus. Donec mattis rutrum nisi, ut hendrerit mauris <a href="#">ornare blandit</a>.</p>
+					<h2>Ich bin ein Zwischentitel</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet erat tincidunt, maximus magna eget, porta quam. Maecenas consequat fermentum odio sit amet tincidunt. Duis nec nisl mi. Mauris vestibulum purus lorem, quis sollicitudin enim tristique sit amet. Integer ultricies dui libero, nec tempor neque porttitor et. Donec hendrerit, purus et lacinia tincidunt, lorem purus commodo sem, et tincidunt velit lacus vitae neque. Integer neque mauris, volutpat sed scelerisque sit amet, maximus vel metus. Donec mattis rutrum nisi, ut hendrerit mauris ornare blandit.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet erat tincidunt, maximus magna eget, porta quam. Maecenas consequat fermentum odio sit amet tincidunt. Duis nec nisl mi. Mauris vestibulum purus lorem, quis sollicitudin enim tristique sit amet. Integer ultricies dui libero, nec tempor neque porttitor et. Donec hendrerit, purus et lacinia tincidunt, lorem purus commodo sem, et tincidunt velit lacus vitae neque. Integer neque mauris, volutpat sed scelerisque sit amet, maximus vel metus. Donec mattis rutrum nisi, ut hendrerit mauris ornare blandit.</p>
+					<h3>Ich bin ein Untertitel</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet erat tincidunt, maximus magna eget, porta quam. Maecenas consequat fermentum odio sit amet tincidunt. Duis nec nisl mi. Mauris vestibulum purus lorem, quis sollicitudin enim tristique sit amet. Integer ultricies dui libero, nec tempor neque porttitor et. Donec hendrerit, purus et lacinia tincidunt, lorem purus commodo sem, et tincidunt velit lacus vitae neque. Integer neque mauris, volutpat sed scelerisque sit amet, maximus vel metus. Donec mattis rutrum nisi, ut hendrerit mauris ornare blandit.</p>
+				</div>
+			</div>
+		</article>
+
+		<br /><br /><br /><br />
+
+	</section><!-- /end section content -->
+
+	<footer id="footer" class="container-fluid px-0">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-lg">
+						<strong>Hund im Auto</strong><br />
+						<p>Herrenwisstrasse 30<br />CH-8180 Bülach</p>
+						<div class="container-fluid g-0 coordinates">
+							<div class="row">
+								<div class="col-3">Telefon</div>
+								<div class="col"><a href="tel:+41448750222">+41 44 875 02 22</a></div>
+							</div>
+							<div class="row">
+								<div class="col-3">E-Mail</div>
+								<div class="col"><a href="mailto:info@hundimauto.ch">info@hundimauto.ch</a></div>
+							</div>
+						</div>
+						<ul class="social-media">
+							<li><a href="/" target="_blank" rel="no-follow"><img src="assets/images/facebook-icon.svg" alt="" height="30" /></a></li>
+							<li><a href="/" target="_blank" rel="no-follow"><img src="assets/images/vimeo-icon.svg" alt=""  height="30" /></a></li>
+						</ul>
+					</div>
+					<div class="col-md-12 col-lg-3">
+						<ul class="footer-nav">
+							<li><a href="/">Produkte nach Mass</a></li>
+							<li><a href="/">Herstellung</a></li>
+							<li><a href="/">Zubehör</a></li>
+							<li><a href="/">Preise</a></li>
+							<li><a href="/">Offerte</a></li>
+						</ul>
+					</div>
+					<div class="col-md-12 col-lg-3">
+						<ul class="footer-meta-nav">
+							<li><a href="/">Impressum</a></li>
+							<li><a href="/">Datenschutz</a></li>
+							<li><a href="/">AGB</a></li>
+						</ul>
+					</div>
+					<div class="col-md-12 col-lg-2 logo">
+						<a href="/"><img src="assets/images/HiA-Sticker50mm-mit-weissem-Rand-OKfix.svg" alt="" /></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						&copy;2024 Hund im Auto
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</footer><!-- end section footer -->
+
+
+
+	<div id="debug">
+	</div>
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
