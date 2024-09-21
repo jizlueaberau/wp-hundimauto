23743932f6aa9f549e140ffc07d55a1522dbbed2
Michael Kuck <michaelkuck@MinivonMichael.fritz.box>
2024-09-21 12:12:40 +0200

    Cleaning up

--- functions.php
+++ functions.php
@@ -56,7 +56,6 @@ function hundimauto_assets() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments') ) {
 		wp_enqueue_script( 'comment-reply');
 	}
-
 }
 add_action( 'wp_enqueue_scripts', 'hundimauto_assets' );
 
