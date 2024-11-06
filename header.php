43a4c5871a1dd741b85e460eabce4477074fcc0a
Michael Kuck <michaelkuck@MinivonMichael.fritz.box>
2024-11-06 14:10:25 +0100

    Embed GTM and changing modal headers

--- header.php
+++ header.php
@@ -9,7 +9,13 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
+<head><!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-55V2MMQ');</script>
+<!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <title><?php echo get_the_title(); ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
@@ -22,7 +28,10 @@
     <?php wp_head(); ?>
 
 </head>
-<body <?php body_class( get_theme_mod( 'theme_setting') ); ?>>
+<body <?php body_class( get_theme_mod( 'theme_setting') ); ?>><!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55V2MMQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <header id="header">
         <div id="theme">
