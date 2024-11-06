2802bd0dce5166964fdedf34218669275b79f511
Michael Kuck <michaelkuck@MinivonMichael.fritz.box>
2024-11-06 15:40:46 +0100

    Adding CookieBot CMP to Header

--- header.php
+++ header.php
@@ -9,13 +9,30 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head><!-- Google Tag Manager -->
+<head><!-- Google Consent Mode --><script data-cookieconsent="ignore">
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments)
+    }
+    gtag("consent", "default", {
+        ad_personalizatioin: "denied",
+        ad_storage: "denied",
+        ad_user_data: "denied",
+        analytics_storage: "denied",
+        personalization_storage: "denied",
+        security_storage: "granted",
+        wait_for_udpate: 500
+    });
+    gtag("set", "ads_data_redaction", true);
+    gtag("set", "url_passthrough", true);
+</script><!-- End Google Consent Mode --><!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-55V2MMQ');</script>
-<!-- End Google Tag Manager -->
+<!-- End Google Tag Manager --><!-- CookieBot CMP -->
+<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="51476768-74bb-40f6-b429-bd2e5e06313c" data-blockingmode="auto" type="text/javascript"></script><!-- End CookieBot CMP -->
     <meta charset="utf-8">
     <title><?php echo get_the_title(); ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
